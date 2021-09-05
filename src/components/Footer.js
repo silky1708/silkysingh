39e14cf678e58f207f576a9c09ce7ac41e1b4f54
Silky Singh <silky1708@gmail.com>
2021-09-06 02:26:25 +0530

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component{
             <Styles>
             <div className="row no-gutters copyright p-3" style={{background:'#DCDCDC', color:'black', marginTop:'20vh'}}>
                 <BiCopyright size={15}/> <div className="">Copyright 2021, Silky Singh. All rights reserved.</div>
-                <div className="ml-auto"><span style={{color:'gray', fontSize:'14px'}}>Last updated: Aug 2021</span></div>
+                <div className="ml-auto"><span style={{color:'gray', fontSize:'14px'}}>Last updated: Sep 2021</span></div>
             </div>
             </Styles>
         )}}
